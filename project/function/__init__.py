8763515d4373dfd16d07e00012db97fd4ccd2aca
LazyKeru <killian.allaire@hotmail.com>
2022-01-04 17:24:46 +0100

    feat: added new functions too __init__.py

--- project/function/__init__.py
+++ project/function/__init__.py
@@ -1,5 +1,7 @@
 # __init__.py
 
+from .standarize_data import standarize_data
+from .train_test_split_local import train_test_split_local
 from .feature_extraction import feature_extraction
 from .load_data import load_data
 from .trace_signal import trace_signal
