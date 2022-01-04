b280dca7e6bee62e420b6dd4a06f13311a4731df
LazyKeru <killian.allaire@hotmail.com>
2022-01-04 14:13:23 +0100

    fix: trace_signal.py is now working

--- project/function/trace_signal.py
+++ project/function/trace_signal.py
@@ -6,27 +6,27 @@ import matplotlib.pyplot as plt
 
 # sensor == accelerometer or gyroscope
 def trace_signal(df,sensor,action,subject,experiment):
-    if ((sensor==gyr)||(sensor==gyroscope)):
+    if ((sensor=="gyr") or (sensor=="gyroscope")):
         sensor="gyr"
-    elif ((sensor==acc)||(sensor==accelerometer)):
+    elif ((sensor=="acc") or (sensor=="accelerometer")):
         sensor="acc"
     else:
         sys.exit("error: trace_signal is expecting one of two sensor")
 
     # df.loc[row_indexer,column_indexer]
-    x = df.loc[(df["action"] == action) && (df["subject"] == subject) && (df["experiment"] == experiment), df[sensor + "_x"]].values
-    y = df.loc[(df["action"] == action) && (df["subject"] == subject) && (df["experiment"] == experiment), df[sensor + "_y"]].values
-    z = df.loc[(df["action"] == action) && (df["subject"] == subject) && (df["experiment"] == experiment), df[sensor + "_z"]].values
+    x = df.loc[(df["action"] == action) & (df["subject"] == subject) & (df["experiment"] == experiment), sensor + "_x"].values
+    y = df.loc[(df["action"] == action) & (df["subject"] == subject) & (df["experiment"] == experiment), sensor + "_y"].values
+    z = df.loc[(df["action"] == action) & (df["subject"] == subject) & (df["experiment"] == experiment), sensor + "_z"].values
 
     # fetch the number of values we will place
-    nb = len(signal_x)
+    nb = len(x)
     # calculating the time axis for which to place the evolution of the different axis (x,y,z)
-    time_axis = [x * 0.02 for x in range(0, nb_point)]
+    time_axis = [x * 0.02 for x in range(0, nb)]
 
-    plt.title(sensor: {sensor}, action: {str(action)}, subject: {str(subject)}, experiment: {str(experiment)}')
+    plt.title("sensor: {sensor}, action: {str(action)}, subject: {str(subject)}, experiment: {str(experiment)}")
 
     plt.xlabel('Time (s)')
-    
+
     plt.plot(time_axis, x, 'r')
     plt.plot(time_axis, y, 'g')
     plt.plot(time_axis, z, 'b')
