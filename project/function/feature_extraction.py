09c4b47b31c5ecfcb22d609039717054eb02c11f
LazyKeru <killian.allaire@hotmail.com>
2022-01-04 15:18:49 +0100

    feat: added feature_extraction function which copies the function of TheoTaffin/AADA

---
+++ project/function/feature_extraction.py
@@ -0,0 +1,34 @@
+# feature_extraction
+#   Here is a list of attributes which can be extracted:
+#   mean, root mean square, standard deviation, median, quartiles,
+#   asymmetry coefficient, Kurtosis, entropy, energy, maxima and minima, coefficient of
+#   correlation, histogram, zero crossings, number of peak occurrences.
+
+def feature_extraction(df):
+    # calcul un vecteur pour chaque action humaine
+    actions_features = {}
+    capteur_features = {}
+    for subject in range(1,df.max(subject)):
+        for experiment in range(1,df.max(experiment)):
+            for action in range(1,df.max(action)):
+                for signal in df.columns[0:6]:
+                    #fetching the vector of the signal
+                    vector = df.loc[(df['action'] == action) & (df['subject'] == subject) & (df['experience'] == experience), [capteur]]
+                    #extracting the features fo the capteur
+                    capteur_features[f"mean_{capteur}"] = float(vector.mean())
+                    capteur_features[f"std_{capteur}"] = float(vector.std())
+                    capteur_features[f"median_{capteur}"] = float(vector.median())
+                    capteur_features[f"mad_{capteur}"] = float(vector.mad())
+                    capteur_features[f"first_quant_{capteur}"] = float(vector.quantile(q=0.25))
+                    capteur_features[f"third_quant_{capteur}"] = float(vector.quantile(q=0.75))
+                    capteur_features[f"min_{capteur}"] = float(vector.min())
+                    capteur_features[f"max_{capteur}"] = float(vector.max())
+                    capteur_features["action"] = action
+                # adding the features of an action
+                features[(subject, experience, action)] = capteur_features
+                # reseting the capteur_features for the next action
+                capteur_features
+    # Remove missing values
+    feature_df = pd.DataFrame.from_dict(features).dropna(axis=1)
+    return feature_df
+    pass
