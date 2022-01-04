35cf4d1acc51dc6a5be4d59751c32295994f5326
LazyKeru <killian.allaire@hotmail.com>
2022-01-04 17:13:16 +0100

    fix: changed experience to experiment

--- project/function/load_data.py
+++ project/function/load_data.py
@@ -14,7 +14,7 @@ def load_data(path):
     # column 6 : id of the subject
     # column 7 : id of the experiment
     # column 8 : id of the action
-    columns_name = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z', 'subject', 'experience', 'action']
+    columns_name = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z', 'subject', 'experiment', 'action']
 
     # All the data will be put in an array with each item having the following shape : ([A][B][C][D][E][F][G][H][I])
     data_array = np.empty(shape=(1, 9))
@@ -28,14 +28,14 @@ def load_data(path):
         mat_contents = sio.loadmat(mat_fname)
         mat_data = mat_contents[name_variable]
 
-        # each file as a naming convention from which we can extract the id of the subject, of the experience and of the action
-        # idAction_idSubject_idExperience
+        # each file as a naming convention from which we can extract the id of the subject, of the experiment and of the action
+        # idAction_idSubject_idExperiment
         subject = np.full((mat_data.shape[0], 1), int(file_name.split('_')[1][1:]))
-        experience = np.full((mat_data.shape[0], 1), int(file_name.split('_')[2][1:]))
+        experiment = np.full((mat_data.shape[0], 1), int(file_name.split('_')[2][1:]))
         action = np.full((mat_data.shape[0], 1), int(file_name.split('_')[0][1:]))
 
         # Join a sequence of arrays along an existing axis
-        temp_array = np.concatenate([mat_data, subject, experience, action], axis=1)
+        temp_array = np.concatenate([mat_data, subject, experiment, action], axis=1)
         # Join a sequence of arrays along an existing axis
         data_array = np.concatenate((data_array, temp_array), axis=0)
 
