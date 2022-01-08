5642fd56077f64f0f3bcf641751cb30f6f2dc97d
LazyKeru <killian.allaire@hotmail.com>
2022-01-08 14:32:07 +0100

    docs: added the description for the functions load_data

--- project/function/load_data.py
+++ project/function/load_data.py
@@ -6,7 +6,12 @@ import os
 import pandas as pd
 
 def load_data(path):
-
+    """
+    loads a dataset of matlab files, with the following naming convention : idAction_idSubject_idExperiment
+    it tranforms the dataset into a DataFrame.
+    :param str path: path to the folder containing all the matlab files
+    :return: DataFrame of the dataset
+    """
     files_list = os.listdir(path)[1:]
     # Our Dataframe will be organized as followed :
     # columns 0-2 : accelerometer (x,y,z)
@@ -15,10 +20,8 @@ def load_data(path):
     # column 7 : id of the experiment
     # column 8 : id of the action
     columns_name = ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z', 'subject', 'experiment', 'action']
-
     # All the data will be put in an array with each item having the following shape : ([A][B][C][D][E][F][G][H][I])
     data_array = np.empty(shape=(1, 9))
-
     for file_name in files_list:
         print("Adding " + file_name + " to our data_array ")
         name_variable = 'd_iner'
@@ -27,18 +30,15 @@ def load_data(path):
         mat_fname=(os.path.join(path, file_name))
         mat_contents = sio.loadmat(mat_fname)
         mat_data = mat_contents[name_variable]
-
         # each file as a naming convention from which we can extract the id of the subject, of the experiment and of the action
         # idAction_idSubject_idExperiment
         subject = np.full((mat_data.shape[0], 1), int(file_name.split('_')[1][1:]))
         experiment = np.full((mat_data.shape[0], 1), int(file_name.split('_')[2][1:]))
         action = np.full((mat_data.shape[0], 1), int(file_name.split('_')[0][1:]))
-
         # Join a sequence of arrays along an existing axis
         temp_array = np.concatenate([mat_data, subject, experiment, action], axis=1)
         # Join a sequence of arrays along an existing axis
         data_array = np.concatenate((data_array, temp_array), axis=0)
-
     # deletes the first column
     data_array = np.delete(data_array, 0, 0)
     df = pd.DataFrame(data_array,  columns=columns_name)
