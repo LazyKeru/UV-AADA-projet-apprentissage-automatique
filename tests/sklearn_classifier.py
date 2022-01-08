69ec2b06986348e26c0ead473a6e0f4fdbc22ff0
LazyKeru <killian.allaire@hotmail.com>
2022-01-08 15:12:47 +0100

    test: simplified the sklearn_classifier test to go faster

--- tests/sklearn_classifier.py
+++ tests/sklearn_classifier.py
@@ -2,7 +2,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import project.function as pf
 
 
@@ -17,4 +16,26 @@ extracted_data=pf.feature_extraction(data)
 
 x_train, x_test, y_train, y_test = pf.train_test_split_local(extracted_data)
 
-pf.sklearn_classifier(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test)
+names = [
+    "MLPClassifier", # Neural network models
+    "SVC", # Support Vector Machines
+]
+
+classifiers = [
+    MLPClassifier(),
+    SVC(),
+]
+
+parameters = [
+    {
+        'hidden_layer_sizes': [100], # default : 100
+        'max_iter': [600, 800, 1000], # default : 200
+        # Many more config : https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
+    },
+    {
+        'C': [0.5, 1.0, 1.5, 2.0], # default=1.0
+        # Many more config : https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+    }
+]
+
+pf.sklearn_classifier(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test,names=names,classifiers=classifiers,parameters=parameters)
